93c5d9ca34c9ca7c1a2c6056777f87b7cb66e121
cszatma <christopherszatmary@gmail.com>
2017-10-14 16:35:08 -0400

    Added Configs and Tests.

---
+++ Tests/QuickPersistTests/QuickPersistTests.swift
@@ -0,0 +1,23 @@
+//
+//  QuickPersistTests.swift
+//  QuickPersist
+//
+//  Created by cszatma on 2017-10-10.
+//  Copyright © 2017 QuickPersist. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import QuickPersist
+
+class QuickPersistTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(QuickPersist().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
